82ede1185eca867dac347f858bb26ece9811af3d
AR <shit@is.nice>
2021-03-19 23:24:39 -0700

    its js now

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = require('express')
+app.use((req,res) => {
+    if (req.path === '/') {
+        res.sendFile(__dirname + '/index.html')
+    }
+    else {
+        res.send('not found')
+        res.status(404)
+    }
+})
+app.listen(3000, () => {
+    console.log('Ready')
+})
