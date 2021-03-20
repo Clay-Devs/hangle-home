152263a4016585ac38155118eadeb9fc32e35943
AR <hijsgeiprygfirh@blobchat.ml>
2021-03-20 06:37:02 +0000

    works again

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ app.use((req,res) => {
     if (req.path === '/') {
         res.sendFile(__dirname + '/index.html')
     }
+    else if (req.path.startsWith('/assets/')) {
+      res.sendFile(__dirname+req.path)
+    }
     else {
         res.send('not found')
         res.status(404)
